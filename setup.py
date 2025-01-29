21313889dcffe27fd8142701449a78521c93971a
Michael Bianco <iloveitaly@gmail.com>
2025-01-29 08:32:22 -0700

    fix github link

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setuptools.setup(
     description="Start a TryCloudflare Tunnel with a context manager.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/UWUplus/soldni/python-cloudflare-tunnel",
+    url="https://github.com/soldni/python-cloudflare-tunnel",
     classifiers=[
         "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: Apache Software License",
